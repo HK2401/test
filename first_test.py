c11ac0c8f431f6d9142716318bc1a90f527ca51c
HK2401 <wikidman_55@hotmail.com>
2020-10-25 21:52:21 +0000

    Add files via upload

---
+++ first_test.py
@@ -0,0 +1,31 @@
+import pytest
+import allure
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+def test_SetUp():
+    global driver
+    driver = webdriver.Chrome('C:/Users/Hares/Desktop/Prestashop/Drivers/chromedriver')   
+
+@pytest.mark.xfail(condition=lambda: True, reason='this test is expecting failure')
+def test_verify_logo2():  
+    driver.get("http://demo.prestashop.com/#/en/front")
+    try:
+        elem = driver.find_element_by_id("logfo").is_displayed()
+        if elem ==True:
+            assert True
+            
+    except Exception as e:
+        print("there was an exception %s", str(e))
+        assert False
+
+def f():
+    raise SystemExit(1)
+
+@allure.severity(allure.severity_level.NORMAL)
+def test_mytest():
+    with pytest.raises(SystemExit):
+        f()
+@allure.severity(allure.severity_level.TRIVIAL)
+def test_tearDown():
+    driver.close()
---
+++ test_menu_navigation.py
@@ -0,0 +1,39 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+def test_setUp():
+    global driver
+    driver = webdriver.Chrome('C:/Users/Hares/Desktop/Prestashop/Drivers/chromedriver')
+
+def test_verify_Menu():
+    driver.get("http://demo.prestashop.com/#/en/front")
+    try:
+        cloths = driver.find_element_by_id("category-3").is_displayed()
+        if clothes ==True:
+            assert True
+            
+    except Exception as e:
+        print("there was an exception %s", str(e))
+        assert False
+
+    try:
+        accessories = driver.find_element_by_id("category-6").is_displayed()
+        if accessories ==True:
+            assert True
+            
+    except Exception as e:
+        print("there was an exception %s", str(e))
+        assert False
+
+    try:
+        art = driver.find_element_by_id("category-9").is_displayed()
+        if art ==True:
+            assert True
+            
+    except Exception as e:
+        print("there was an exception %s", str(e))
+        assert False
+
+def test_tearDown():
+    driver.close()
